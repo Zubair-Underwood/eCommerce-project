97435a429c5f9273a6f8d8451041d16880e6784d
Your Name <youremail@example.com>
2023-09-25 05:04:20 +0500

    learning

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SellerService } from '../services/seller.service';
+import { Router } from "@angular/router";
+import { SignUp } from '../data-type';
 
 @Component({
   selector: 'app-seller-auth',
@@ -6,11 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seller-auth.component.css']
 })
 export class SellerAuthComponent implements OnInit {
-  constructor() { }
+  constructor(private seller: SellerService, private router: Router) { }
 
   ngOnInit(): void { }
 
-  signUp(data: object): void {
-    console.warn (data)
+  signUp(data: SignUp): void {
+    this.seller.userSignUp(data).subscribe((result) => {
+      if (result) {
+        this.router.navigate(["seller-home"])
+      }
+
+    });
   }
 }
